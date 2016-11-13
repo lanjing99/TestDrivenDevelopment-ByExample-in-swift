a4ee2d79ba368f35119180e555cf9becc5e85f91
lanjing <84336951@qq.com>
2016-11-13 10:33:33 +0800

    add initial project

---
+++ Currency/Currency/Currency.h
@@ -0,0 +1,19 @@
+//
+//  Currency.h
+//  Currency
+//
+//  Created by lanjing on 11/13/16.
+//  Copyright © 2016 lanjing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Currency.
+FOUNDATION_EXPORT double CurrencyVersionNumber;
+
+//! Project version string for Currency.
+FOUNDATION_EXPORT const unsigned char CurrencyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Currency/PublicHeader.h>
+
+
---
+++ Currency/Currency/Currency.swift
@@ -0,0 +1,9 @@
+//
+//  Currency.swift
+//  Currency
+//
+//  Created by lanjing on 11/13/16.
+//  Copyright © 2016 lanjing. All rights reserved.
+//
+
+import Foundation
